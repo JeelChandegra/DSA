d634a7acfe28e0a2461b6e5883af7dc2757c888b
jeel <chandegrajeel@gmail.com>
2025-12-09 21:17:04 +0530

    Add solution: Binary Search

---
+++ 2025-12-09_Binary_Search.cpp
@@ -0,0 +1,29 @@
+/*
+Problem: Binary Search
+Link: https://neetcode.io/problems/binary-search/question
+Date: 2025-12-09
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) 
+            return mid;
+
+        if (target < nums[mid])
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+
+    return -1;
+
+    }
+};
+
