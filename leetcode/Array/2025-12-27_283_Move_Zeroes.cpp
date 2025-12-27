7250343e9cce2765d26d65735bceb9093d50fe39
jeel <chandegrajeel@gmail.com>
2025-12-27 16:04:35 +0530

    Add solution: 283. Move Zeroes

---
+++ leetcode/Array/2025-12-27_283_Move_Zeroes.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: 283. Move Zeroes
+Link: https://leetcode.com/problems/move-zeroes/
+Date: 2025-12-27
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0; 
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+
+        
+    }
+};
